7fb4d731752f79a5aedcdf75f6a877f964e0531f
Kewaun Cain <kewaunscain@gmail.com>
2022-06-06 19:56:37 -0400

    FullStack Coffe App

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : "mongodb+srv://coffeApp:Q1O84bB37XvX2DWN@cluster0.gjpz1wr.mongodb.net/?retryWrites=true&w=majority",
+    'db' : "coffeeChat"
+};
